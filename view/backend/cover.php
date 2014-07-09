fc2e9a36a3f4b26002c78db9adde99b0ba7ee06a
Okeowo <okeowoaderemi@gmail.com>
2014-07-09 15:32:23 +0100

    Logic to Check if empty row

--- view/backend/cover.php
+++ view/backend/cover.php
@@ -8,14 +8,22 @@
        </tr>
 
        </thead>
-       <?php foreach($data as $item): ?>
+       <?php
+       if(count($data)):
+       foreach($data as $item): ?>
        <tbody>
        <tr>
            <td><img src='<?php echo $item->getImages(true);?>'/></td>
            <td><a class="btn btn-primary" href="<?php echo ipActionUrl(array('aa'=>'ImageAlbum.makeCover','album'=>$id,'imageID'=>$item->getID())); ?>">Make Album Cover</a></td>
        </tr>
        </tbody>
-       <?php endforeach;?>
+       <?php endforeach;
+       else:
+       ?>
+    <h5>No Images attached in this Album</h5>
+    <?php
+        endif;
+    ?>
    </table>
 
 </div>
