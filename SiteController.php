8493e6a3c515e2901256f0e28d63ae9ac9abf3d5
Okeowo <okeowoaderemi@gmail.com>
2014-07-09 15:34:43 +0100

    JavaScript to alert user on user before deleteing the data

---
+++ SiteController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: x64
+ * Date: 7/8/14
+ * Time: 3:32 PM
+ */
+
+namespace Plugin\ImageAlbum;
+
+
+class SiteController {
+
+} 
---
+++ assets/js/AlbumPage.js
@@ -0,0 +1,6 @@
+/**
+ * Created by x64 on 7/9/14.
+ */
+define(function () {
+    return {};
+});
