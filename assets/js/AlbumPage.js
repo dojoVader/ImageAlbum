5dccb9526ff87aa4e60f0dba4e7e37b834025a86
Okeowo <okeowoaderemi@gmail.com>
2014-07-15 00:05:32 +0100

    Added AlbumPage interaction code

--- assets/js/AlbumPage.js
+++ assets/js/AlbumPage.js
@@ -1,6 +1,20 @@
 /**
  * Created by x64 on 7/9/14.
  */
-define(function () {
-    return {};
+define(['dojo/on'],function (on) {
+
+    return {
+        bindDelete:function(){
+            on($('.deleteAlbum'),'click',function(e){
+            //Throw a Confirm Box to ask before allowing Page to Delete
+                var Choice=window.confirm("Are you really certain of this, this will Delete Attached Images Also");
+                if(Choice){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            });
+        }
+    };
 });
